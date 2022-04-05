6bdba453fb641ba93523acea7442520194269b21
jsthomas <jsthomas1288@gmail.com>
2022-04-05 15:16:03 -0500

    Created the list of user's repos

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
 // This is the section that will contain all of my information
 const overview = document.querySelector(".overview");
+// This is the section where the list of repos will go
+const repoList = document.querySelector(".repo-list");
 
 const username = "jsthomas1288";
 
 const getUserInfo = async function () {
   const request = await fetch(`https://api.github.com/users/${username}`);
-  const data = await request.json();
-  console.log(data);
-  displayUserInfo(data);
+  const userData = await request.json();
+  console.log(userData);
+  displayUserInfo(userData);
   console.log(overview);
 };
 
 getUserInfo();
 
-const displayUserInfo = function (data) {
+const displayUserInfo = function (userData) {
   const userInfo = document.createElement("user-info");
   userInfo.innerHTML = `
-  <figure>
-    <img alt = "user avatar" src=${data.avatar_url} />
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${data.name} </p>
-    <p><strong>Bio:</strong> ${data.bio} </p>
-    <p><strong>Location:</strong> ${data.location} </p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos} </p>
-  </div>`;
+  <div class = "user-info">
+    <figure>
+        <img alt = "user avatar" src=${userData.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${userData.name} </p>
+        <p><strong>Bio:</strong> ${userData.bio} </p>
+        <p><strong>Location:</strong> ${userData.location} </p>
+        <p><strong>Number of public repos:</strong> ${userData.public_repos} </p>
+    </div>
+</div>`;
   overview.append(userInfo);
+  getUserRepos();
+};
+
+const getUserRepos = async function () {
+  const requestRepo = await fetch(
+    `https://api.github.com/users/${username}/repos?per_page=100&sort=updated`
+  );
+  const repoData = await requestRepo.json();
+  console.log(repoData);
+  displayUserRepos(repoData);
+  console.log(repoList);
+};
+
+const displayUserRepos = function (repoData) {
+  for (const repo of repoData) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <div class = "repo">
+        <h3>${repo.name}</h3>
+    </div>`;
+    repoList.append(li);
+  }
 };
